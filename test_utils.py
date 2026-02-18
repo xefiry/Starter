223e9ae0248527d463df1a3f5d094b9955dc1e59
xefiry <188508735+xefiry@users.noreply.github.com>
2026-02-18 22:20:42 +0100

    created utils.py with test_utils.py

---
+++ test_utils.py
@@ -0,0 +1,66 @@
+import pytest
+
+import utils
+
+
+@pytest.mark.parametrize(
+    "input,expected",
+    [
+        ([], True),
+        (["arg"], True),
+        (["arg1", "arg2"], True),
+        (["arg1", 3], False),
+        ([3], False),
+        ([False], False),
+        ([6.5, "Ok ?"], False),
+        ([["Ok ?"]], False),
+    ],
+)
+def test_is_str_list(input, expected):
+    assert utils.is_str_list(input) == expected
+
+
+@pytest.mark.parametrize(
+    "input,expected",
+    [
+        (None, []),
+        ([], []),
+        ("arg", ["arg"]),
+        (["arg"], ["arg"]),
+        (["arg1", "arg2"], ["arg1", "arg2"]),
+    ],
+)
+def test_process_args(input, expected):
+    assert utils.process_args(input) == expected
+
+
+@pytest.mark.parametrize("input", [[3], 3, False])
+def test_process_args_exceptions(input):
+    with pytest.raises(TypeError):
+        utils.process_args(input)
+
+
+@pytest.mark.parametrize(
+    "input,expected",
+    [
+        (["argA1", "argB1"], [["argA1"], ["argB1"]]),
+        ([["argA1"], ["argB1"]], [["argA1"], ["argB1"]]),
+        (
+            [["argA1", "argA2"], ["argB1", "argB2"]],
+            [["argA1", "argA2"], ["argB1", "argB2"]],
+        ),
+        (
+            [["argA1", "argA2"], ["argB1"], "argC1"],
+            [["argA1", "argA2"], ["argB1"], ["argC1"]],
+        ),
+    ],
+)
+def test_process_args_list(input, expected):
+    assert utils.process_args_list(input) == expected
+
+
+# TODO: write it
+@pytest.mark.parametrize("input", [[3], 3, False])
+def test_process_args_list_exception(input):
+    with pytest.raises(TypeError):
+        utils.process_args_list(input)
---
+++ utils.py
@@ -0,0 +1,63 @@
+def is_str_list(input_list: list) -> bool:
+    """Check if the given list is a string list
+
+    Args:
+        input_list (list): A list to check
+
+    Returns:
+        bool: True if the list contains only strings (str) or is empty
+    """
+    for item in input_list:
+        if not isinstance(item, str):
+            return False
+    return True
+
+
+def process_args(input: list[str] | str | None) -> list[str]:
+    """Processes an input into a list of arguments usable by subprocess.run/subprocess.Popen
+
+    Args:
+        input (list[str] | str | None): the input to be processed
+
+    Raises:
+        TypeError: if the input contains invalid type
+
+    Returns:
+        list[str]: the processed list, ready to be used by subprocess.run/subprocess.Popen
+
+    Examples:
+        process_args(null) = []
+        process_args([]) = []
+        process_args("arg") = ["arg"]
+        process_args(["arg"]) = ["arg"]
+        process_args(["arg1", "arg2"]) = ["arg1", "arg2"]
+    """
+    result = []
+
+    if input is None:
+        result = []
+    elif isinstance(input, str):
+        result = [input]
+    elif isinstance(input, list) and is_str_list(input):
+        result = input
+    else:
+        raise TypeError
+
+    return result
+
+
+# TODO: write it
+def process_args_list(input) -> list[list[str]]:
+    """Processes an input into a list of list of arguments usable to loop
+    and call by subprocess.run/subprocess.Popen
+
+    Returns:
+        list[list[str]]: the processed list, ready to be looped to use by subprocess.run/subprocess.Popen
+
+    Examples:
+    process_args_list(["argA1", "argB1"]) = [["argA1"], ["argB1"]]
+    process_args_list([["argA1"], ["argB1"]]) = [["argA1"], ["argB1"]]
+    process_args_list([["argA1", "argA2"], ["argB1", "argB2"]]) = [["argA1", "argA2"], ["argB1", "argB2"]]
+    process_args_list([["argA1", "argA2"], ["argB1"], "argC1"]) = [["argA1", "argA2"], ["argB1"], ["argC1"]]
+    """
+    return [["TODO"]]
